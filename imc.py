2a36301a71cd26476b41823530046c5fea180ac1
marciocandidos@gmail.com <seu e-mail>
2023-01-12 15:39:55 -0300

    IMC

---
+++ imc.py
@@ -0,0 +1,23 @@
+
+peso = float(input('Digite seu peso: '))
+altura =float(input('Digite sua altura: '))
+imc = peso / altura ** 2 
+print('Seu imc é: ', imc)
+
+
+if imc < 16:
+	print("Magreza grave")
+elif imc < 17:
+	print("Magreza moderada")
+elif imc < 18.5:
+	print("Magreza leve")
+elif imc < 25:
+	print("Saudável")
+elif imc < 30:
+	print("Sobrepeso")
+elif imc < 35:
+	print("Obesidade Grau I")
+elif imc < 40:
+	print("Obesidade Grau II (severa)")
+else:
+	print("Obesidade Grau III (mórbida)")
